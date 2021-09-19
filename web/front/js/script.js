3518f0eb8cf609bc743ece8aa39f263cfecfbc47
Timofey Drenkov <45731970+Vindori@users.noreply.github.com>
2021-09-19 18:57:53 +0300

    Update script.js

--- web/front/js/script.js
+++ web/front/js/script.js
@@ -1,15 +1,4 @@
 $(window).ready(function () {
-    $('#app').css({
-        borderRadius: "35px",
-        margin: "20px 0px 20px 0px",
-        backgroundColor: "black",
-        display: "flex",
-        justifyContent: "center",
-        paddingLeft: "0",
-        paddingRight: "0",
-        width: "220px",
-        height: "60px"
-    }).css('background-color', 'red').text('salam alekum').css('background-image', 'url(../)');
 
     $('#app').click(function(){
         data = {

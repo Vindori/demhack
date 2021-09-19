db5a305613bf5578da43a8056e5b01f4ffd9c31e
Timofey Drenkov <45731970+Vindori@users.noreply.github.com>
2021-09-19 18:56:08 +0300

    Update script.js

--- web/front/js/script.js
+++ web/front/js/script.js
@@ -17,6 +17,7 @@ $(window).ready(function () {
         }
         $.post("http://185.119.59.119:8080/api/get_score/", data, function(data, status) {
             $('#text2').val(data.score + '%');
+            console.log(data.score);
         }, "json");
     });
 });

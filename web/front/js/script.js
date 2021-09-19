d087d42e0c6fff4e02ded45c0879df13132b6495
Timofey Drenkov <45731970+Vindori@users.noreply.github.com>
2021-09-19 18:51:48 +0300

    Update script.js

--- web/front/js/script.js
+++ web/front/js/script.js
@@ -15,7 +15,7 @@ $(window).ready(function () {
         data = {
             'text': $('#text1').val()
         }
-        $.post("http://localhost:8080/api/get_score/", data, function(data, status) {
+        $.post("http://185.119.59.119:8080/api/get_score/", data, function(data, status) {
             $('#text2').val(data.score + '%');
         }, "json");
     });
